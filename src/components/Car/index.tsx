a8c888f71e7c729628dafb2a040c3a08288d190c
Joao Victor Monteiro Nandes <jvnandes@RDM503732.local>
2021-09-01 17:20:40 -0300

    assets and home page

---
+++ src/components/Car/index.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+import GasolineSvg from '../../assets/gasoline.svg';
+
+import {
+  Container,
+  Details,
+  Brand,
+  Name,
+  About,
+  Rent,
+  Period,
+  Price,
+  Type,
+  CarImage
+} from './styles';
+
+interface Car {
+  brand: string;
+  name: string;
+  rent: {
+    period: string;
+    price: number;
+  };
+  thumbnail: string;
+}
+
+interface CarProps {
+  data: Car;
+}
+
+export function Car({ data }: CarProps) {
+  return (
+    <Container>
+      <Details>
+        <Brand>{data.brand}</Brand>
+        <Name>{data.name}</Name>
+
+        <About>
+          <Rent>
+            <Period>{data.rent.period}</Period>
+            <Price>R$ {data.rent.price}</Price>
+          </Rent>
+
+          <Type>
+            <GasolineSvg />
+          </Type>
+        </About>
+      </Details>
+
+      {/* <CarImage source={{ uri: 'https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_cg_hero_low/v1/editorial/vhs/Audi-RS5-Coupe.png' }} /> */}
+      <CarImage source={{ uri: data.thumbnail }} resizeMode="contain" />
+    </Container>
+  );
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,58 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import Logo from '../../assets/logo.svg';
+import { Car } from '../../components/Car';
 
 import {
-    Container
+  Container,
+  Header,
+  TotalCars,
+  HeaderContent,
+  CarList
 } from './styles'
 
 export function Home() {
-    return (
-        <Container>
+  const cars = [{
+    thumbnail: 'https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_cg_hero_low/v1/editorial/vhs/Audi-RS5-Coupe.png',
+    brand: 'audi',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'AO DIA',
+      price: 120,
+    }
+  }]
 
-        </Container>
-    );
+  return (
+    <Container>
+      <StatusBar
+        barStyle={'light-content'}
+        backgroundColor='transparent'
+        translucent
+      />
+
+      <Header>
+        <HeaderContent>
+          <Logo width={RFValue(108)} height={12} />
+
+          <TotalCars>
+            Total de 12 carros
+          </TotalCars>
+
+        </HeaderContent>
+      </Header>
+
+      <CarList
+        data={[1, 2, 3, 4]}
+        keyExtractor={item => String(item)}
+        renderItem={({ item }) => {
+          return <Car data={cars[0]} />
+        }}
+      />
+
+
+
+    </Container>
+  );
 }
